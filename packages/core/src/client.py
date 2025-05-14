42a8d01c879745545696dbdbff47070de05ee335
SeiyaKobayashi <eng@seiya.work>
2025-05-14 17:10:01 +0900

    Refactor client

--- packages/core/src/client.py
+++ packages/core/src/client.py
@@ -1,6 +1,6 @@
 from eth_account.signers.local import LocalAccount
 from web3 import Account, HTTPProvider, Web3
-from web3.middleware import ExtraDataToPOAMiddleware
+from web3.middleware import ExtraDataToPOAMiddleware, SignAndSendRawMiddlewareBuilder
 
 from interfaces import ISdkClient
 from utils import get_default_rpc_endpoint
@@ -9,11 +9,9 @@ class SdkClient(ISdkClient):
     """SDK client.
 
     Attributes:
-        rpc_endpoint (str): RPC_endpoint
         w3 (Web3): Configured web3 instance
         account (LocalAccount): Local account
     """
-    rpc_endpoint: str
     w3: Web3
     account: LocalAccount
 
@@ -34,27 +32,31 @@ class SdkClient(ISdkClient):
             NetworkNotSupported: If the specified network is not supported by the SDK
         """
         rpc_endpoint = get_default_rpc_endpoint(chain_name, network_name)
+        account = Account.from_key(private_key)
 
-        self.rpc_endpoint = rpc_endpoint
-        self.w3 = Web3(HTTPProvider(rpc_endpoint)).middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-        self.account = Account.from_key(private_key)
+        w3 = Web3(HTTPProvider(rpc_endpoint))
+        w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+        w3.middleware_onion.inject(SignAndSendRawMiddlewareBuilder.build(account), layer=0)
+
+        self.w3 = w3
+        self.account = account
 
     def set_default_provider(self, chain_name: str, network_name: str) -> Web3:
         rpc_endpoint = get_default_rpc_endpoint(chain_name, network_name)
 
-        self.rpc_endpoint = rpc_endpoint
-        self.w3 = Web3(HTTPProvider(rpc_endpoint)).middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+        self.w3 = Web3(HTTPProvider(rpc_endpoint))
 
         return self.w3
 
     def set_custom_provider(self, rpc_endpoint: str) -> Web3:
-        self.rpc_endpoint = rpc_endpoint
         self.w3 = Web3(HTTPProvider(rpc_endpoint))
 
         return self.w3
 
     def set_account(self, private_key: str) -> LocalAccount:
-        self.account = Account.from_key(private_key)
+        account = Account.from_key(private_key)
+        self.w3.middleware_onion.replace(SignAndSendRawMiddlewareBuilder.build(self.account), SignAndSendRawMiddlewareBuilder.build(account))
+        self.account = account
 
         return self.account
 
